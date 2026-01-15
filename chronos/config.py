bd76917af405e0ab897887804705ad3cda4bb826
Farhan Mir <farhan.mirshah@gmail.com>
2026-01-15 15:21:54 -0500

    Fix CI import error and implement GitHub download count

--- chronos/config.py
+++ chronos/config.py
@@ -2,7 +2,7 @@
 
 # The unique completion code that Gemini will output when done
 # This is injected via --append-system-prompt (if supported) or manually
-COMPLETION_CODE = "<<<CHRONOS_TASK_COMPLETE_GEMINI>>>"
+CHRONOS_TASK_COMPLETE_GEMINI = "<<<CHRONOS_TASK_COMPLETE_GEMINI>>>"
 
 # Session file location (relative to project directory)
 SESSION_DIR = ".chronos"
